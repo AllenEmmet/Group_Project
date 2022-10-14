d3ebf32d38906287f33fce3f7e27440084825f5b
JohnCarter <gjohncartet@protonmail.com>
2022-10-13 19:05:23 -0600

    Boilerplate added comments to some files Main.js in views Dashboard.js in components let me know what youre thinkin @AllenEmmet

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,6 @@
+/*
+  * Dashboard for our site
+  * Holds: 
+  * User info, goals, calories, exercises
+  * These then can be broken down into their seperate componenets
+  */
---
+++ client/src/views/Main.js
@@ -0,0 +1,5 @@
+/*
+  * Here Emmet we can put our Main view, reuse components 
+  * and manage state better, lets communicate how we want
+  * our frontend to function
+  */
