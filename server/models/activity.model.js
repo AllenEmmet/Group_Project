b54560c9f7f5506d510c2b2fd8798546deefe549
WillTurner-ST <williamturner321@gmail.com>
2022-10-15 21:31:08 -0400

    Updated Model

---
+++ server/models/activity.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ActivitySchema = new mongoose.Schema({
+    exercise: {
+        type:String,
+        enum:{
+            values:[
+                'Lunges',
+                'Pushups',
+                'Squats',
+                'Dumbbell Curls',
+                'Bench Press',
+                'Pull-ups',
+                'Dips',
+                'Bicep Curls',
+            ],
+            
+        required:[true,"A class is required"]
+    },
+    duration: {
+        type: String,
+        required: [true, "Activity Duration is required"],
+        minLength: [3, "Activity's duration must have at least 3 characters"],
+    },
+
+
+}
+
+});
+
+module.exports = mongoose.model("Activity", ActivitySchema); 
